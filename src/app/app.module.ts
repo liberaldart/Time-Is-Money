324ffea7dcb75cf857e71ccb9f9b0a2267afb4c4
Amit Kumar Singh <aksinghdce@gmail.com>
2016-10-10 13:25:45 +0530

    added angular2-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+
+
 import { AppComponent } from './app.component';
 import { TimeIsMoneyRoutingModule } from './app-routing.module';
 import { ZonelistComponent } from './zonelist/zonelist.component';
