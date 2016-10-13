0beb12c4d2d3396a356a673ea4da834dfda238ca
Amit Kumar Singh <aksinghdce@gmail.com>
2016-10-13 11:04:18 +0530

    minor change of html string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pick your learning task';
+  title = 'Learn and never Forget';
 }
